8e201c5110e5357cd6e0bca7f5a4ed0aa87c9491
Vicky  Tee <vttak9@gmail.com>
2021-01-13 23:44:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,22 +3,28 @@
 #through all numbers from 2 to n-1 and for every number check if it divides n.
 # If we find any number that divides, we return false.
 
-
-
-
-
-
-def prime?(i)
-
-Gombo.new(2...i-1)
-while i >= 2
-if  Gombo.each {|i|  i % Gom}
-return false
-else
-  return true
-  if i < 0
-    return false
+def prime?(integer)
+    return false if integer < 2
+    (2..integer - 1).each do |x|
+        if (integer % x) == 0
+            return false
+        end
+    end
+    true
 end
-end
-end
-end
+
+
+
+
+#def prime?(i)
+
+#Gombo.new(2...i-1)
+#while i >= 2
+#if  Gombo.each {|i|  i % Gom}
+#return false
+#else
+  #return true
+  #if i < 0
+    #return false
+ 
+
